84783933b555f58f66fbb4ad9fe1f8068a50a9d7
Pythobit boy <79314246+Pythobit@users.noreply.github.com>
2021-08-13 20:19:16 +0530

    Update 9-Filter-data-from-dataframe.py

--- Pandas/9-Filter-data-from-dataframe.py
+++ Pandas/9-Filter-data-from-dataframe.py
@@ -2,3 +2,24 @@
 Two ways - Label based indexing, position based indexing
 """
 
+# for label indexing
+df6.loc['735 Dolores St':'3995 23rd St','City':'Name']
+
+
+# for a specific address city
+df6.loc['735 Dolores St','City']
+
+
+# for all cities
+df6.loc[:,'City']
+
+
+list(df6.loc[:,'City'])
+
+
+# for position indexing, we use iloc
+df6.iloc[1:3, 1:3]
+
+# here 3 will not be included as it is upper bound exclusive
+
+df6.iloc[3, 1:4]
